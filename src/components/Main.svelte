87e9c82cd91f92d7276335d9dac580add667e1e5
Joceliyn <jocevaldez98@gmail.com>
2024-06-17 19:34:37 -0700

    added to main and added image

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,30 @@
-Hello, welcome to my portfolio! I am a newbe on here so please be kind.
+<script>
+
+</script>
+
+<main class='flex-flex-col flex-1 p-4'>
+    <section id='introPage' class='grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14'>
+      <div class='flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10'>
+        <h2 class='font-semibold text-4xl sm:text-5xl md:text-6xl'>
+            Hello! I'm <span class='poppins text-violet-400'>Joceliyn</span> Valdez <br/> Full Stack 
+            <span class='poppins text-violet-400'>Developer</span>
+        </h2>
+        <p class='text-base sm:text-lg md:text-xl'>
+            My <span class='text-violet-400'>main tech</span> includes JavaScript (React, Node.js, Express), and SQL (NoSQL). <br/>
+             I also have experience with HTML and CSS. Currently, I am learning Python and SvelteKit. <br/>
+             I am a <span class='text-violet-400'>fast learner</span> and I am always looking for a new challenge!
+        </p>
+        <a class='blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950
+        cursor-pointer' 
+        href='#' 
+        target='_blank'>
+            <div class='absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200'
+        />
+        <h4 class='relative z-9'>Get in touch &rarr;</h4>
+        </a>
+      </div>
+      <div class='relative shadow-2xl grid place-items-center'>
+        <img src={'src/lib/assets/joceimage.jpg'} alt='Profile image' class='object-cover z-[2] max-h-[70vh]'/>
+      </div>
+    </section>
+</main>
